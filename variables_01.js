6a43017c6b98ee720c1d0341df35103624de8268
Haddi <haddi.dev@gmail.com>
2025-12-20 01:37:53 +0500

    learn about var, let and const

---
+++ variables_01.js
@@ -0,0 +1,21 @@
+//var, let, const 
+
+//not use
+console.log(name)
+
+var name = "Haddi"
+
+
+//  can use
+let age = 25
+
+console.log(age)
+
+
+const pi = 3.14
+const country = "PK";
+const courncy = "PKR";
+
+pi = 3.14159
+
+console.log(pi)
