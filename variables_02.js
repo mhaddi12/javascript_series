b685a29dbf8b931ccddd8a03f963a4bb19990e57
Haddi <haddi.dev@gmail.com>
2025-12-23 22:49:13 +0500

    learn about variables

---
+++ variables_02.js
@@ -0,0 +1,29 @@
+let userName = "HaddiDev"
+console.log(typeof userName)
+console.log(userName)
+
+let userAge = 25
+console.log(typeof userAge)
+console.log(userAge)
+
+let isLoggedIn = true
+console.log(typeof isLoggedIn)
+console.log(isLoggedIn)
+
+let userAddress = 1;
+console.log(typeof userAddress)
+console.log(userAddress)
+
+let userIndex = [0, 1, 2, 3, 4]
+// console.log(typeof userIndex)
+console.log(userIndex instanceof Array)
+console.log(userIndex)
+console.log(userIndex[2])
+
+let userProfile = {
+    name: "HaddiDev",
+    age: 25,
+}
+console.log(typeof userProfile)
+console.log(userProfile)
+console.log(userProfile.name)
